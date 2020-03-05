34fa99d093a63957497570d254b103da39e8daaf
serra-vp <vincent.serra@boom.camp>
2020-03-05 10:07:33 +0800

    Changes in tp#4

--- Day-3/1/index.php
+++ Day-3/1/index.php
@@ -6,50 +6,18 @@
   <title>Toy Problem 4</title>
 </head>
 <body>
-  <?php
-    
-    //Answer in Procedural
-    $x=10;$y=7;
-    echo "
-      {$x} + {$y} = " .($x+$y). " <br/>
-      {$x} - {$y} = " .($x-$y). " <br/>
-      {$x} * {$y} = " .($x*$y). " <br/>
-      {$x} / {$y} = " .($x/$y). " <br/>
-      {$x} % {$y} = " .($x%$y). " <br/>
-    ";
-    
-    /*
-    //Answer in OOP 
-    class Arithmetic{
-      public function add($x,$y){
-        $this->sum = $x + $y;
-        echo "{$x} + {$y} = {$this->sum}";
-      }
-      public function subtract($x,$y){
-        $this->difference = $x - $y;
-        echo "{$x} - {$y} = {$this->difference}";
-      }
-      public function multiply($x,$y){
-        $this->product = $x * $y;
-        echo "{$x} * {$y} = {$this->product}";
-      }
-      public function divide($x,$y){
-        $this->quotient = $x / $y;
-        echo "{$x} / {$y} = {$this->quotient}";
-      }
-      public function modulo($x,$y){
-        $this->remainder = $x % $y;
-        echo "$x % {$y} = {$this->remainder}";
-      }
-    }
-    $x=10;$y=7;
-    $result = new Arithmetic();
-    echo $result->add($x,$y)."<br>";
-    echo $result->subtract($x,$y)."<br>";
-    echo $result->multiply($x,$y)."<br>";
-    echo $result->divide($x,$y)."<br>";
-    echo $result->modulo($x,$y)."<br>";
-    */
+  <?php 
+    $x=10;$y=7;$z=0;
+    $z = $x + $y;
+    echo "$x + $y = $z <br/>";
+    $z = $x - $y;
+    echo "$x - $y = $z <br/>";
+    $z = $x * $y;
+    echo "$x * $y = $z <br/>";
+    $z = $x / $y;
+    echo "$x / $y = $z <br/>";
+    $z = $x % $y;
+    echo "$x % $y = $z <br/>";
   ?>
 </body>
 </html>
